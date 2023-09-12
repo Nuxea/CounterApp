d8bfac4d24a82666a11425ab47aed8fbe5801c2f
Nuxea <lescieux.am@gmail.com>
2023-09-12 20:26:07 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# CounterApp
+
+Simple Counter app to learn Vue.js
+
+[![Vue.js](https://img.shields.io/badge/-Vue.js-000?&logo=Vue.js&logoColor=4FC08D)](https://vuejs.org/)
+[![Vite](https://img.shields.io/badge/-Vite-000?&logo=Vite&logoColor=4FC08D)](https://vitejs.dev/)
+
+*See [Vite Configuration Reference](https://vitejs.dev/config/).*
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup>
+  import {ref} from "vue";
+
+  let count = ref(0)
+</script>
+
+<template>
+  <main>
+    <container class="container">
+      <h4>The current count is...</h4>
+      <h1>{{count}}</h1>
+      <div class="buttons">
+        <button @click="count--">-</button>
+        <button @click="count++">+</button>
+      </div>
+
+    </container>
+  </main>
+</template>
+
+<style scoped>
+main {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.container {
+  text-align: center;
+}
+
+.buttons {
+  display: flex;
+  justify-content: space-evenly;
+  gap: 30px;
+}
+
+button {
+  padding: 10px;
+  width: 100px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: var(--vt-c-text-dark-1);
+}
+</style>
